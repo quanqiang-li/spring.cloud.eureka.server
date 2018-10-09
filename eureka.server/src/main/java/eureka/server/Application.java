814644fbfed4da5a5dd3855a139f32a9f7fc24da
aisino-liqq <liquanqiang@aisino.com>
2018-10-09 17:42:16 +0800

    服务端配置

--- eureka.server/src/main/java/eureka/server/Application.java
+++ eureka.server/src/main/java/eureka/server/Application.java
@@ -1,15 +1,25 @@
 package eureka.server;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class Application {
+	
+	private static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+    	ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+    	String[] activeProfiles = run.getEnvironment().getActiveProfiles();
+    	log.info("spring boot 使用的profile为{}",Arrays.deepToString(activeProfiles));
+    	
     }
 
 }
